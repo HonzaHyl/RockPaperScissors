870fc357d231ecb1dd54241ca7cf2a020a2b5585
Jan Hýl <99046227+HonzaHyl@users.noreply.github.com>
2023-08-15 17:58:31 +0200

    Create playRound function
    Function takes two parameters: playerSelection and computerSelection
    Return result of the round

--- game.js
+++ game.js
@@ -9,3 +9,41 @@ function getComputerChoice() {
     return randomItem;
 };
 
+function playRound(playerSelection, computerSelection) {
+
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    switch (player) {
+        case "rock":
+            if (computer === "paper"){ 
+                return "You lose! Paper beats rock!"
+            } else if (computer === "scissors") {
+                return "You win! Scissors beats rock!"
+            } else {
+                return "It's a tie!"
+            };
+            break;
+        
+        case "paper":
+            if (computer === "scissors"){ 
+                return "You lose! Scissors beats paper!"
+            } else if (computer === "rock") {
+                return "You win! Paper beats rock!"
+            } else {
+                return "It's a tie!"
+            };
+            break;
+
+        case "scissors":
+            if (computer === "rock"){ 
+                return "You lose! Rock beats scissors!"
+            } else if (computer === "paper") {
+                return "You win! Scissors beats paper!"
+            } else {
+                return "It's a tie!"
+            };
+            break;
+    }
+};
+
